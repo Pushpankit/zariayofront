d0102fdc129bb29e72a19973057433021463b14a
Pushpankit <pushpankitsingh963@gmail.com>
2025-06-02 19:15:34 +0530

    Deploy-ready update

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { CartProvider } from "./context/CartContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/zarizyo-frontend">
       <CartProvider>
         <App />
       </CartProvider>
